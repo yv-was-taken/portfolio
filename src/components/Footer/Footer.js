121da745ad7ae2c5f403be00691877e8fcdc3cf7
ywvlfy <ywvlfy@gmail.com>
2022-02-07 16:29:07 -0600

    footer ui

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,15 @@ import "./Footer.css";
 export default function Footer() {
   return(
 
-    <div className="footer-container">Footer</div>
+    <div className="footer-container"><div> <div className="footer-text">Highlights</div></div>
+      <div className="footer-links-container">
+
+        <div className="highlight">almost 9,000 followers on twitter <br/> (I'm good at shitposting)</div>
+        <div className="highlight">Intro to TA series</div>
+        <div className="highlight">Full Stack: onepixel.</div>
+        <div className="highlight">Frontend Development @UXDProtocol</div>
+      </div>
+
+    </div>
   )
 }
