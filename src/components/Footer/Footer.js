8d1bd5b5a09d79cfdffd77766812ad01f5dec003
ywvlfy <ywvlfy@gmail.com>
2022-02-11 12:24:40 -0600

    did the thing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,15 +7,42 @@ export default function Footer() {
     <div className="footer-container">
       <div className="footer-links-container">
 
-        <div className="highlight">Dev: OnlyOnePixel</div>
 
         <div className="highlight">
           <div>
             Frontend Development @UXDProtocol
+            <div>
+              August 2021 - Current
+            </div>
 
-          </div>
-          <div>
-            August 2021 - Current
+            <div className='work-example-container'>
+              <div className="work-example">
+                <div className='work-example-title'><a href="https://app.uxd.fi" target="_blank">app.uxd.fi</a></div>
+                <div className="work-example-list">
+                  <div>React</div>
+                  <div>Typescript</div>
+                  <div>Scss</div>
+                </div>
+              </div>
+              <div className="work-example">
+                <div className='work-example-title'><a href="https://ido.uxd.fi" target="_blank">ido.uxd.fi</a></div>
+                <div className="work-example-list">
+                  <div>NEXT JS</div>
+                  <div>Typescript</div>
+                  <div>Scss</div>
+                </div>
+              </div>
+              <div className="work-example">
+                <div className='work-example-title'><a href="https://uxd.fi" target="_blank">uxd.fi</a></div>
+                <div className="work-example-list">
+                  <div>HTML</div>
+                  <div>CSS</div>
+                  <div style={{opacity: 0}}> .</div>
+                </div>
+              </div>
+
+            </div>
+
           </div>
 
         </div>
